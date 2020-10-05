f3a4e6d70c634d8fc0d2e7c648b5a5d2ae4c84d3
Swizec Teller <swizec@swizec.com>
2020-10-05 07:14:58 -0700

    Solution with SSM

--- exercise-3/src/send-sms.ts
+++ exercise-3/src/send-sms.ts
@@ -1,11 +1,35 @@
 import twilio from "twilio";
+import { SecretsManager } from "aws-sdk";
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const accountToken = process.env.TWILIO_ACCOUNT_TOKEN;
+const ssm = new SecretsManager({
+    region: "us-east-1", // make sure this matches your region
+});
 
-const client = twilio(accountSid, accountToken);
+async function getKeys() {
+    try {
+        const secret = await ssm
+            .getSecretValue({
+                SecretId: "twilioWorkshopKeys",
+            })
+            .promise();
+
+        if (secret?.SecretString) {
+            return JSON.parse(secret?.SecretString);
+        } else {
+            throw new Error("Couldn't get secrets");
+        }
+    } catch (err) {
+        console.error("Error retrieving secrets");
+        console.error(err);
+        throw err;
+    }
+}
 
 export const handler = async () => {
+    const { TWILIO_ACCOUNT_SID, TWILIO_ACCOUNT_TOKEN } = await getKeys();
+
+    const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_ACCOUNT_TOKEN);
+
     const msg = await client.messages.create({
         body: "hello from lambda",
         to: "+16505375963",
