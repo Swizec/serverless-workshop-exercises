115e8c349235399f6b34e5016f97123e4f846e60
Swizec Teller <swizec@swizec.com>
2020-10-04 17:35:06 -0700

    Solution for exercise 2

--- exercise-2/happy-day.ts
+++ exercise-2/happy-day.ts
@@ -1,2 +1,27 @@
 // copypasta your Netlify function here
 // add fixes to make TypeScript happy
+
+import { APIGatewayEvent } from "aws-lambda";
+
+export const handler = async (event: APIGatewayEvent) => {
+    let { name, emoji } = event.queryStringParameters as {
+        name: string;
+        emoji: string;
+    };
+
+    if (!name && event.body) {
+        name = JSON.parse(event.body).name;
+    }
+    if (!emoji && event.body) {
+        emoji = JSON.parse(event.body).emoji;
+    }
+
+    if (!name) {
+        name = event.pathParameters?.name as string;
+    }
+
+    return {
+        statusCode: 200,
+        body: `Hello ${name}, beautiful day today ${emoji}`,
+    };
+};
